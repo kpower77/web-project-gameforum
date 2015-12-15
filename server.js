2a4847753b6b53479600c43f77381a7c1efede5e
kpower77 <kpower77@gmail.com>
2015-12-15 22:14:41 +0000

    Changed to using JSON, added in modules for body-parser and js2xmlparser

--- server.js
+++ server.js
@@ -2,32 +2,90 @@
 var http = require('http');
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var xslt = require('node_xslt');
 var fs = require("fs");
+var js2xmlparser = require("js2xmlparser");
 
 var router = express();
 var server = http.createServer(router);
 
 
 router.use(express.static(path.resolve(__dirname, 'client')));
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(bodyParser.json());
 
+
+//Get request to display index.html
 router.get('/', function(req, res){
   res.render('index');
 });
 
- //This will write to the file test1.txt and will replace the file with the string specified
-  fs.appendFile('test1.txt', 'This is some additional text I have appended to the file', function(err){
-    if(err) throw err;
-    console.log('File has been written to');
-  });
+
+// Get request to send back the JSON file
+router.get('/get/json', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var obj = JSON.parse(fs.readFileSync('topic.json', 'utf8'));
+  res.end(JSON.stringify(obj));
+});
 
 
-router.get('/xml', function(req, res) {
+// HTML produced by XSL Transformation
+router.get('/get/html', function(req, res) {
+  
+  // Read in XML and XSL files
   var stylesheet = xslt.readXsltFile('topic.xsl');
   var doc = xslt.readXmlFile('topic.xml');
+  
+  // Apply transformation
   var result = xslt.transform(stylesheet, doc, []);
+  
+  // Render the result
   res.send(result);
+  
 });
+
+// POST request to add to JSON & XML files
+router.post('/post/json', function(req, res) {
+
+  // Function to read in a JSON file, add to it & convert to XML
+  function appendJSON(obj) {
+
+    // Read in a JSON file
+    var JSONfile = fs.readFileSync('topic.json', 'utf8');
+
+    // Parse the JSON file in order to be able to edit it 
+    var JSONparsed = JSON.parse(JSONfile);
+
+    // Add a new record into topic array within the JSON file    
+    JSONparsed.topic.push(obj);
+
+    // Beautify the resulting JSON file
+    var JSONformated = JSON.stringify(JSONparsed, null, 4);
+
+    // Write the updated JSON file back to the system 
+    fs.writeFileSync('topic.json', JSONformated);
+
+    // Convert the updated JSON file to XML     
+    var XMLformated = js2xmlparser("topic", JSONformated);
+
+    // Write the resulting XML back to the system
+    fs.writeFileSync('topic.xml', XMLformated);
+
+  }
+
+  // Call appendJSON function and pass in body of the current POST request
+  appendJSON(req.body);
+  
+  // Re-direct the browser back to the page, where the POST request came from
+  res.redirect('back');
+
+});
+
+
+
+
+
 
 
 
